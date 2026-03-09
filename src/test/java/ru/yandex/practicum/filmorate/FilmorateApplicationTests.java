50d70a4be38682dde84228e410e94afcd1c5e91b
aiymellaia <aiymdaurenkyzy@yandex.kz>
2026-03-09 20:14:37 +0500

    chore: fix checkstyle filename mismatch

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FilmControllerTest {
+class FilmorateApplicationTests {
 	private FilmController controller;
 	private Validator validator;
 
